8d632f29a9e1a37ebd2534da20f4fa397fc7c583
Kiran Jagtap <karan27288@fmail.com>
2020-10-17 04:05:58 +0530

    Committing servlet code

---
+++ ServletContextDemo/src/main/java/webapp/GenericServletDemo.java
@@ -0,0 +1,59 @@
+package webapp;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class GenericServletDemo extends GenericServlet {
+
+    private UserValidationService userValidationService = new UserValidationService();
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+        System.out.println("---------post");
+
+        ServletContext context = servletRequest.getServletContext();
+		
+		System.out.println("Context param app name" + context.getInitParameter("appname"));
+        
+		System.out.println("Context param app name" + context.getInitParameter("cart"));
+		
+		
+        boolean isValid = userValidationService.isValidUser(servletRequest.getParameter("username"),
+                servletRequest.getParameter("password"));
+        if (isValid) {
+
+            PrintWriter out = servletResponse.getWriter();
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Yahoo!!!!!!!!</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("Successful login");
+            out.println("</body>");
+            out.println("</html>");
+
+            //      servletRequest.getRequestDispatcher("WEB-INF/views/welcome.jsp").forward(servletRequest, servletResponse);
+        } else {
+
+            PrintWriter out = servletResponse.getWriter();
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Yahoo!!!!!!!!</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("Please try again");
+            out.println("</body>");
+            out.println("</html>");
+
+            //    servletRequest.getRequestDispatcher("WEB-INF/views/login.jsp").forward(servletRequest, servletResponse);
+
+        }
+
+    }
+}
